d7cb355e26d50950827e2c7c252fc825474936c5
Dylan Agiman <dylanagiman@gmail.com>
2019-05-16 22:55:25 -0700

    Updated documentation for teleport function + basic teleport runthrough in main loop

--- piano_mission.py
+++ piano_mission.py
@@ -44,6 +44,14 @@ def teleport(agent_host, teleport_x, teleport_z):
     ---------
     teleport_y is not needed because we will assume a desired
     teleport height of 57, which is ground level
+
+    To teleport to a specific note, from the list:
+    notes = ['F_sharp_3','G3','G_sharp_3','A3','A_sharp_3','B3','C4','C_sharp_4','D4','D_sharp_4','E4','F4','F_sharp_4', \
+            'G4','G_sharp_4','A4','A_sharp_4','B4','C5','C_sharp_5','D5','D_sharp_5','E5','F5','F_sharp_5']
+
+    To teleport to a note at index i, call 
+        teleport(agent_host, -13, i)
+    where i is the index of the note + 1.5
     '''
 
     print("\ntrying a teleport")
@@ -60,8 +68,8 @@ def teleport(agent_host, teleport_x, teleport_z):
         if not good_frame and world_state.number_of_video_frames_since_last_state > 0:
             frame_x = world_state.video_frames[-1].xPos
             frame_z = world_state.video_frames[-1].zPos
-            print("Current frame_x: {}".format(frame_x))
-            print("Current frame_z: {}".format(frame_z))
+            # print("Current frame_x: {}".format(frame_x))
+            # print("Current frame_z: {}".format(frame_z))
             if math.fabs(frame_x - teleport_x) < 0.001 and math.fabs(frame_z - teleport_z) < 0.001:
                 good_frame = True
                 end_frame = timer()
@@ -195,13 +203,16 @@ print("Mission running ", end=' ')
 #agent_host.sendCommand("tp -14 57 0")
 
 # Loop until mission ends:
+# teleporting to all of the notes, test run, to be changed
+i=1.5
 while world_state.is_mission_running:
     print(".", end="")
-    time.sleep(0.1)
-    teleport(agent_host, -13, 1)
+    time.sleep(0.5)
+    teleport(agent_host, -13, i)
     world_state = agent_host.getWorldState()
     for error in world_state.errors:
         print("Error:",error.text)
+    i += 1
 
 print()
 print("Mission ended")
